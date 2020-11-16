45e27d3ef38e68fa728f2544a44872e6627873af
pervinpashazade <ParvinKHp@code.local>
2020-11-16 08:08:19 +0400

    @swr package added

--- src/components/skeletons/VacancyCardSkeleton.js
+++ src/components/skeletons/VacancyCardSkeleton.js
@@ -1,10 +1,123 @@
 import React from 'react'
 import Skeleton from 'react-loading-skeleton'
 
-function VacancyCardSkeleton() {
+const VacancyCardSkeleton = () => {
     return (
-        <div>
-            
+        <div className="row mt-5 ml-3">
+            <div className="col-md-6 mb-4">
+                <h4>
+                    <Skeleton duration={1} height={25} width={450} />
+                </h4>
+                <p>
+                    <Skeleton height={25} width={360} />
+                </p>
+                <p>
+                    <Skeleton height={25} width={230} />
+                </p>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    <li>
+                        <Skeleton height={25} width={290} />
+                    </li>
+                    <li>
+                        <Skeleton height={25} width={"25%"} />
+                    </li>
+                </ul>
+            </div>
+            <div className="col-md-6 mb-4">
+                <h4>
+                    <Skeleton duration={1} height={25} width={450} />
+                </h4>
+                <p>
+                    <Skeleton height={25} width={360} />
+                </p>
+                <p>
+                    <Skeleton height={25} width={230} />
+                </p>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    <li>
+                        <Skeleton height={25} width={290} />
+                    </li>
+                    <li>
+                        <Skeleton height={25} width={"25%"} />
+                    </li>
+                </ul>
+            </div>
+            <div className="col-md-6 mb-4">
+                <h4>
+                    <Skeleton duration={1} height={25} width={450} />
+                </h4>
+                <p>
+                    <Skeleton height={25} width={360} />
+                </p>
+                <p>
+                    <Skeleton height={25} width={230} />
+                </p>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    <li>
+                        <Skeleton height={25} width={290} />
+                    </li>
+                    <li>
+                        <Skeleton height={25} width={"25%"} />
+                    </li>
+                </ul>
+            </div>
+            <div className="col-md-6 mb-4">
+                <h4>
+                    <Skeleton duration={1} height={25} width={450} />
+                </h4>
+                <p>
+                    <Skeleton height={25} width={360} />
+                </p>
+                <p>
+                    <Skeleton height={25} width={230} />
+                </p>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    <li>
+                        <Skeleton height={25} width={290} />
+                    </li>
+                    <li>
+                        <Skeleton height={25} width={"25%"} />
+                    </li>
+                </ul>
+            </div>
+            <div className="col-md-6 mb-4">
+                <h4>
+                    <Skeleton duration={1} height={25} width={450} />
+                </h4>
+                <p>
+                    <Skeleton height={25} width={360} />
+                </p>
+                <p>
+                    <Skeleton height={25} width={230} />
+                </p>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    <li>
+                        <Skeleton height={25} width={290} />
+                    </li>
+                    <li>
+                        <Skeleton height={25} width={"25%"} />
+                    </li>
+                </ul>
+            </div>
+            <div className="col-md-6 mb-4">
+                <h4>
+                    <Skeleton duration={1} height={25} width={450} />
+                </h4>
+                <p>
+                    <Skeleton height={25} width={360} />
+                </p>
+                <p>
+                    <Skeleton height={25} width={230} />
+                </p>
+                <ul style={{ listStyleType: "none", padding: 0 }}>
+                    <li>
+                        <Skeleton height={25} width={290} />
+                    </li>
+                    <li>
+                        <Skeleton height={25} width={"25%"} />
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/containers/vacancies/Vacancies.js
+++ src/containers/vacancies/Vacancies.js
@@ -1,12 +1,17 @@
 import Axios from 'axios'
+// import useSWR from 'swr'
 import React, { useState, useEffect, useMemo } from 'react'
+import VacancyCardSkeleton from '../../components/skeletons/VacancyCardSkeleton'
 import VacancyCard from '../../components/vacancyCard/VacancyCard'
 import { VacancyContext } from '../../context/Context'
 import './vacancies.css'
 
 const Vacancies = () => {
 
+    // const {data} = useSWR('https://devjobscore.prospectsmb.com/v1/vacancies')
+
     const [vacancies, setVacancies] = useState([])
+    const [loading, setLoading] = useState(false)
     const providerValue = useMemo(() => ({ vacancies, setVacancies }), [vacancies, setVacancies])
 
     const notRendered = !vacancies || (vacancies && vacancies.length === 0)
@@ -22,44 +27,49 @@ const Vacancies = () => {
     }
 
     useEffect(() => {
-        getAllVacancies();
+        setLoading(true);
+        const timing = setTimeout(() => {
+            getAllVacancies();
+            setLoading(false);
+        }, 2200);
+        return () => clearTimeout(timing);
     }, [])
 
     return (
         <VacancyContext.Provider value={providerValue}>
 
+
+            {loading && <VacancyCardSkeleton />}
             <div className="row mt-4">
-                {
-                    vacancies.map((vacancy, index) => {
-                        return (
-                            <div key={index} className="col-md-6">
-                                <VacancyCard
-                                    id={vacancy.id}
-                                    isVip={vacancy.isVip}
-                                    status={vacancy.status}
-                                    viewsCount={vacancy.viewsCount}
-                                    isBookmarked={vacancy.bookmarked}
-                                    createdAt={vacancy.createdAt}
-                                    expiredAt={vacancy.expiredAt}
-                                    canRequest={vacancy.canRequest}
-                                    requested={vacancy.requested}
-                                    title={vacancy.name}
-                                    company={vacancy.companyName}
-                                    position={vacancy.position.name}
-                                    city={vacancy.city.name}
-                                    salary={vacancy.salary}
-                                    currency={!vacancy.currency ? "" : vacancy.currency.name}
-                                    minSalary={vacancy.minSalary}
-                                    maxSalary={vacancy.maxSalary}
-                                    minExperience={vacancy.minExpirience}
-                                    maxExperience={vacancy.maxExpirience}
-                                />
-                            </div>
-                        )
-                    })
-                }
+                {!loading && vacancies.map((vacancy, index) => {
+                    return (
+                        <div key={index} className="col-md-6">
+                            <VacancyCard
+                                id={vacancy.id}
+                                isVip={vacancy.isVip}
+                                status={vacancy.status}
+                                viewsCount={vacancy.viewsCount}
+                                isBookmarked={vacancy.bookmarked}
+                                createdAt={vacancy.createdAt}
+                                expiredAt={vacancy.expiredAt}
+                                canRequest={vacancy.canRequest}
+                                requested={vacancy.requested}
+                                title={vacancy.name}
+                                company={vacancy.companyName}
+                                position={vacancy.position.name}
+                                city={vacancy.city.name}
+                                salary={vacancy.salary}
+                                currency={!vacancy.currency ? "" : vacancy.currency.name}
+                                minSalary={vacancy.minSalary}
+                                maxSalary={vacancy.maxSalary}
+                                minExperience={vacancy.minExpirience}
+                                maxExperience={vacancy.maxExpirience}
+                            />
+                        </div>
+                    )
+                })}
             </div>
-            
+
         </VacancyContext.Provider>
     )
 }
