aab5c9e03d013c9966c0d81cda274158850b6e9f
pervinpashazade <ParvinKHp@code.local>
2020-11-28 23:46:41 +0400

    vacancy reducer & action completed

--- src/containers/vacancies/Vacancies.js
+++ src/containers/vacancies/Vacancies.js
@@ -10,25 +10,30 @@ import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner'
 
 const Vacancies = (props) => {
 
+    console.log(props.vacancies)
+
     useEffect(() => {
-        props.getVacancies()
+
+        if (props.vacancies.length === 0) {
+            props.getVacancies()
+        }
+
     }, [])
 
     return (
 
         <section>
+            {props.isLoading && <LoadingSpinner />}
             <div className="row mt-4">
-                {
-                    props.isLoading ? <LoadingSpinner /> : props.vacancies.map((item, index) => {
-                        return (
-                            <div key={index} className="col-md-6">
-                                <Link to={{ pathname: '/vacancy/' + item.id, vacancyProps: item }}>
-                                    <VacancyCard item={item} />
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
+                {!props.isLoading && props.vacancies.map((item, index) => {
+                    return (
+                        <div key={index} className="col-md-6">
+                            <Link to={{ pathname: '/vacancy/' + item.id, vacancyProps: item }}>
+                                <VacancyCard item={item} />
+                            </Link>
+                        </div>
+                    )
+                })}
             </div>
         </section>
     )
--- src/store/actions/vacancyActions.js
+++ src/store/actions/vacancyActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const getVacancies = (state) => dispatch => {
-    dispatch({type: 'GET_LOADING_TRUE'})
+    dispatch({type: 'GET_VACANCIES_START'})
     axios.get('https://devjobscore.prospectsmb.com/v1/vacancies')
         .then(response => dispatch({ type: 'GET_VACANCIES_SUCCESS', payload: response.data.data }))
         .catch(error => dispatch({type: 'GET_VACANCIES_ERROR', payload: error}))
