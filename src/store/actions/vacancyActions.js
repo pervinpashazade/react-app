74e490a1f4092e66f2dca0e20866708e36f3fcd7
pervinpashazade <ParvinKHp@code.local>
2020-11-29 01:05:47 +0400

    vacancyActions :> names => function()

--- src/store/actions/vacancyActions.js
+++ src/store/actions/vacancyActions.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
 
 export const getVacancies = (state) => dispatch => {
-    dispatch({type: 'LOAD_VACANCIES_REQUEST'})
+    dispatch({ type: LOAD_VACANCIES_REQUEST() })
     axios.get('https://devjobscore.prospectsmb.com/v1/vacancies')
-        .then(response => dispatch({ type: 'LOAD_VACANCIES_SUCCESS', payload: response.data.data }))
-        .catch(error => dispatch({type: 'LOAD_VACANCIES_FAILURE', payload: error}))
+        .then(response => dispatch({ type: LOAD_VACANCIES_SUCCESS(), payload: response.data.data }))
+        .catch(error => dispatch({ type: LOAD_VACANCIES_FAILURE(), payload: error }))
+}
+
+const LOAD_VACANCIES_REQUEST = () => {
+    return 'LOAD_VACANCIES_REQUEST'
+}
+
+const LOAD_VACANCIES_SUCCESS = () => {
+    return 'LOAD_VACANCIES_SUCCESS'
+}
+
+const LOAD_VACANCIES_FAILURE = () => {
+    return 'LOAD_VACANCIES_FAILURE'
 }
